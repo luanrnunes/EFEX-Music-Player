40c86d91a71a7ed904e45837b4287b9958ee5e6a
luanrnunes <luanrobertonunes@hotmail.com>
2022-02-05 15:19:08 -0300

    Arquivos iniciais do projeto

---
+++ src/application/Main.java
@@ -0,0 +1,49 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	private static Scene mainScene;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			ScrollPane scrollPane = loader.load();
+			
+			
+			/*Ajusta o menu para ir ate o fim da tela*/
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
+			mainScene = new Scene(scrollPane);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("EFEX Music Player");
+			primaryStage.show();
+			
+			/*Aplica o CSS*/
+			mainScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/gui/MainViewController.java
@@ -0,0 +1,98 @@
+package gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.function.Consumer;
+
+import application.Main;
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+public class MainViewController implements Initializable {
+
+	@FXML
+	private MenuItem menuItemFile;
+
+	@FXML
+	private MenuItem menuItemConfig;
+
+	@FXML
+	private MenuItem menuItemAbout;
+
+	@FXML
+	public void onMenuItemFileAction() {
+		loadView("/gui/ItemView.fxml", (FileViewController controller) -> {
+			
+			System.out.println("ok");
+		});
+	}
+
+	@FXML
+	public void onMenuItemConfig() {
+		loadView("/gui/Item.fxml", (ConfigViewController controller) -> {
+			System.out.println("ok");
+		});
+	}
+
+	@FXML
+	public void onMenuItemAboutAction() {
+		loadView("/gui/About.fxml", x -> {
+			System.out.println("ok");
+		});
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+
+	}
+
+	/*
+	 * synchronized obriga que todo o processamento da janela seja executado, nao
+	 * permite interromper.
+	 */
+	/*
+	 * Consumert <T> Foi definido o parâmetro a ser executado assim que a janela for
+	 * aberta
+	 */
+	/*
+	 * Evitando criar mais de um loadView, mantendo em apenas 1, recebendo
+	 * parametros diferentes
+	 */
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
+
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+
+			Scene mainScene = Main.getMainScene();
+
+			/* Casting para o VBox da tela principal. Ver FXML... */
+			/* Permite que mantenha a janela aberta junto as janelas filhas */
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+
+			/* Executa a acao dos parametros que foram passados para inicializacao */
+			T controller = loader.getController();
+			initializingAction.accept(controller);
+
+		} catch (IOException e) {
+
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+
+	}
+
+}
