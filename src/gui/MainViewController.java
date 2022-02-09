a1fb9ad95cebfe9a3376b5cdeceb369d7c9e0011
luanrnunes <luanrobertonunes@hotmail.com>
2022-02-09 01:06:30 -0300

    Ajustes de reproducao

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -4,10 +4,13 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Timer;
 import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
+import javafx.beans.binding.Bindings;
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -23,6 +26,7 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class MainViewController implements Initializable {
 
@@ -34,36 +38,42 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	private MenuItem menuItemAbout;
-	
+
 	@FXML
 	private Button playButton;
-	
+
 	@FXML
 	private Button stopButton;
-	
+
 	@FXML
 	private Button playListButton;
-	
+
 	@FXML
 	private Button pauseButton;
-	
+
 	@FXML
 	private Stage stage;
-	
+
 	@FXML
 	private Label actionStatus;
-	
+
+	@FXML
+	private Label mediaTime;
+
 	private String fullPath;
-	
+
 	private MediaPlayer mediaPlayer;
-	
+
 	private Media hit;
 	
+	private String selectedAudio;
+
+	private ChangeListener<Duration> TimeListener;
 
 	@FXML
 	public void onMenuItemFileAction() {
 		loadView("/gui/ItemView.fxml", (FileViewController controller) -> {
-			
+
 			System.out.println("ok");
 		});
 	}
@@ -78,63 +88,79 @@ public class MainViewController implements Initializable {
 	@FXML
 	public void onbtPlayButtonAction() {
 		try {
-		
-		if (mediaPlayer == null) {
-		String selectedAudio = fullPath;
-		hit = new Media(new File(selectedAudio).toURI().toString());
-		mediaPlayer = new MediaPlayer(hit);
-		mediaPlayer.play();
-		}
-		else {
-			mediaPlayer.play();
-		}
+
+			if (mediaPlayer == null) {
+				selectedAudio = fullPath;
+				hit = new Media(new File(selectedAudio).toURI().toString());
+				mediaPlayer = new MediaPlayer(hit);
+				mediaPlayer.play();
+				mediaTime.setText(mediaPlayer.getCurrentTime()+"\n"+mediaPlayer.getCycleDuration());
+			} else {
+				if (mediaPlayer != null) {
+					selectedAudio = fullPath;
+					hit = new Media(new File(selectedAudio).toURI().toString());
+					mediaPlayer.stop();
+					mediaPlayer.play();
+					mediaTime.setText(mediaPlayer.getCurrentTime()+"\n"+mediaPlayer.getCycleDuration());
+				}
 			}
-		catch(Exception e) {
+		}
+		 catch (Exception e) {
 			actionStatus.setText("No song selected!");
+			System.out.println(e.getMessage());
 		}
 		
+
 	}
+
 	@FXML
 	public void onbtStopButtonAction() {
+		selectedAudio = fullPath;
+		hit = new Media(new File(selectedAudio).toURI().toString());
 		mediaPlayer.stop();
+		mediaTime.setText(mediaPlayer.getCurrentTime()+"\n"+mediaPlayer.getCycleDuration());
+		
 	}
-	
+
 	@FXML
 	public void pauseButtonAction() {
+		String selectedAudio = fullPath;
+		hit = new Media(new File(selectedAudio).toURI().toString());
 		mediaPlayer.pause();
+		mediaTime.setText(mediaPlayer.getCurrentTime()+"\n"+mediaPlayer.getCycleDuration());
 	}
-	
+
 	@FXML
 	public void onbtPlaylistAction() {
-		
+
 		FileChooser fileChooser = new FileChooser();
 		File selectedFile = fileChooser.showOpenDialog(null);
 		try {
 			fullPath = selectedFile.getCanonicalPath();
 			System.out.println(fullPath);
 
-		         
 		} catch (IOException e) {
-			
-			actionStatus.setText("Failed getting file path: "+e);
+
+			actionStatus.setText("Failed getting file path: " + e);
 		}
-		 
+
 		if (selectedFile != null) {
-		 
-		    actionStatus.setText("Now playing: " + selectedFile.getName());
+
+			actionStatus.setText("Now playing: " + selectedFile.getName());
+		} else {
+			actionStatus.setText("File selection cancelled.");
 		}
-		else {
-		    actionStatus.setText("File selection cancelled.");
-		}
-	}
 		
+		
+	}
 	
+			
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		
+
 		loadView("/gui/AboutView.fxml", x -> {
-			
+
 		});
 	}
 
